f7afce55dc65778390b05c9a04370527d41ca18c
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-10-07 20:25:55 +0100

    Rework converter tests

--- converter/tests/test_converter.rs
+++ converter/tests/test_converter.rs
@@ -1,55 +1,52 @@
 use converter::convert;
 use pretty_assertions::assert_eq;
-use quick_xml::reader::Reader;
-use serde::Deserialize;
-use std::fs::{self, File};
-use std::path::PathBuf;
-use tempfile::tempdir;
+use tempfile::NamedTempFile;
 
-#[derive(Deserialize)]
-struct TestCase {
-    description: String,
-    input_file: String,
-    expected_output_file: String,
-    extra_cli_args: Option<Vec<String>>,
+fn normalize(s: &str) -> String {
+    s.lines()
+        .map(|line| line.trim_end()) // Trim trailing whitespace from each line
+        .collect::<Vec<_>>()
+        .join("\n") // Join with '\n' to ensure consistent line endings
 }
 
 #[test]
-fn test_converter() -> Result<(), Box<dyn std::error::Error>> {
-    let data_dir = PathBuf::from(".").join("tests").join("data");
-    let test_cases_path = data_dir.join("test_cases.json");
-    let test_cases: Vec<TestCase> = serde_json::from_str(&fs::read_to_string(&test_cases_path)?)?;
+fn test_convert_with_empty_lines() -> Result<(), Box<dyn std::error::Error>> {
+    let input_file = "empty_lines.tsv";
+    let input_file_path = format!("./tests/data/inputs/{input_file}");
+    std::fs::exists(&input_file_path)
+        .expect(&format!("Input file '{}' does not exist", &input_file_path));
 
-    let temp_dir = tempdir()?;
-    for (index, test_case) in test_cases.iter().enumerate() {
-        let output_file = temp_dir.path().join(format!("output_{}.xml", index));
-        File::create(output_file.as_path())?;
+    let expected_output_file = "empty_lines.xml";
+    let expected_output_file_path = format!("./tests/data/expected_outputs/{expected_output_file}");
 
-        let input_file_full_path = data_dir.join(&test_case.input_file);
-        if !input_file_full_path.exists() {
-            return Err(format!("File {:?} does not exist", input_file_full_path).into());
-        }
-        if let Some(args) = &test_case.extra_cli_args {
-            todo!("Pass extra {args:?} to converter");
-        }
-        convert(
-            input_file_full_path.to_str().unwrap(),
-            output_file.to_str().unwrap(),
-            '\t',
-        )?;
+    let actual_output_file = NamedTempFile::new().expect("Failed to create a temp file");
+    let actual_output_file_path = actual_output_file.path().to_str().unwrap();
 
-        let expected_output_contents =
-            fs::read_to_string(data_dir.join(&test_case.expected_output_file))?;
-        let expected_xml = Reader::from_str(&expected_output_contents.trim());
-        let actual_output_contents = fs::read_to_string(&output_file)?;
-        let actual_xml = Reader::from_str(&actual_output_contents.trim());
+    convert(&input_file_path, actual_output_file_path, '\t').expect("Conversion failed");
 
-        assert_eq!(
-            std::str::from_utf8(expected_xml.into_inner()),
-            std::str::from_utf8(actual_xml.into_inner()),
-            "Mismatch in test case: {}",
-            test_case.description
-        );
-    }
+    let expected_output_contents =
+        std::fs::read_to_string(&expected_output_file_path).expect(&format!(
+            "Failed to read expected output file '{}'",
+            &expected_output_file_path
+        ));
+    let actual_output_contents =
+        std::fs::read_to_string(&actual_output_file_path).expect(&format!(
+            "Failed to read actual output file '{}'",
+            &actual_output_file_path
+        ));
+    assert_eq!(
+        normalize(&actual_output_contents),
+        normalize(&expected_output_contents)
+    );
     Ok(())
+}
+
+#[test]
+fn test_convert_with_extra_columns() {
+    todo!("Implement me!")
+}
+
+#[test]
+fn test_convert_with_missing_columns() {
+    todo!("Implement me!")
 }
