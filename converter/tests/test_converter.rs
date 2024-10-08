3926dc28b4d69921c4a9c5ddf08dc5183fa7dcf6
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-10-08 18:58:33 +0100

    Define macro to keep tests DRY

--- converter/tests/test_converter.rs
+++ converter/tests/test_converter.rs
@@ -9,44 +9,55 @@ fn normalize(s: &str) -> String {
         .join("\n") // Join with '\n' to ensure consistent line endings
 }
 
-#[test]
-fn test_convert_with_empty_lines() -> Result<(), Box<dyn std::error::Error>> {
-    let input_file = "empty_lines.tsv";
-    let input_file_path = format!("./tests/data/inputs/{input_file}");
-    std::fs::exists(&input_file_path)
-        .expect(&format!("Input file '{}' does not exist", &input_file_path));
+macro_rules! run_test {
+    ($func:ident, $input_file:expr, $expected_output_file:expr) => {
+        #[test]
+        fn $func() -> Result<(), Box<dyn std::error::Error>> {
+            let input_file_path = format!("./tests/data/inputs/{}", $input_file);
+            std::fs::exists(&input_file_path)
+                .expect(&format!("Input file '{}' does not exist", &input_file_path));
 
-    let expected_output_file = "empty_lines.xml";
-    let expected_output_file_path = format!("./tests/data/expected_outputs/{expected_output_file}");
+            let expected_output_file_path =
+                format!("./tests/data/expected_outputs/{}", $expected_output_file);
 
-    let actual_output_file = NamedTempFile::new().expect("Failed to create a temp file");
-    let actual_output_file_path = actual_output_file.path().to_str().unwrap();
+            let actual_output_file = NamedTempFile::new().expect("Failed to create a temp file");
+            let actual_output_file_path = actual_output_file.path().to_str().unwrap();
 
-    convert(&input_file_path, actual_output_file_path, '\t').expect("Conversion failed");
+            convert(&input_file_path, actual_output_file_path, '\t').expect("Conversion failed");
 
-    let expected_output_contents =
-        std::fs::read_to_string(&expected_output_file_path).expect(&format!(
-            "Failed to read expected output file '{}'",
-            &expected_output_file_path
-        ));
-    let actual_output_contents =
-        std::fs::read_to_string(&actual_output_file_path).expect(&format!(
-            "Failed to read actual output file '{}'",
-            &actual_output_file_path
-        ));
-    assert_eq!(
-        normalize(&actual_output_contents),
-        normalize(&expected_output_contents)
-    );
-    Ok(())
+            let expected_output_contents = std::fs::read_to_string(&expected_output_file_path)
+                .expect(&format!(
+                    "Failed to read expected output file '{}'",
+                    &expected_output_file_path
+                ));
+            let actual_output_contents =
+                std::fs::read_to_string(&actual_output_file_path).expect(&format!(
+                    "Failed to read actual output file '{}'",
+                    &actual_output_file_path
+                ));
+            assert_eq!(
+                normalize(&actual_output_contents),
+                normalize(&expected_output_contents)
+            );
+            Ok(())
+        }
+    };
 }
 
-#[test]
-fn test_convert_with_extra_columns() {
-    todo!("Implement me!")
-}
+run_test!(
+    convert_with_empty_lines,
+    "empty_lines.tsv",
+    "empty_lines.xml"
+);
 
-#[test]
-fn test_convert_with_missing_columns() {
-    todo!("Implement me!")
-}
+run_test!(
+    convert_with_extra_columns,
+    "extra_columns.tsv",
+    "extra_columns.xml"
+);
+
+run_test!(
+    convert_with_missing_columns,
+    "missing_columns.tsv",
+    "missing_columns.xml"
+);
